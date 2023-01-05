baca7c14d9c115b82d7825fe88a2c7ed1903209b
CoopperGuy <crisente235@gmail.com>
2023-01-05 16:12:59 +0900

    asda
    asd

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI; // AI, 내비게이션 시스템 관련 코드를 가져오기
 
 // 적 AI를 구현한다
-public class Enemy : LivingEntity {
+public class Enemy : LivingEntity
+{
     public LayerMask whatIsTarget; // 추적 대상 레이어
 
     private LivingEntity targetEntity; // 추적할 대상
@@ -37,47 +39,125 @@ public class Enemy : LivingEntity {
         }
     }
 
-    private void Awake() {
+    private void Awake()
+    {
         // 초기화
+        pathFinder = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+        enemyAudioPlayer = GetComponent<AudioSource>();
+
+        enemyRenderer = GetComponentInChildren<Renderer>();
+        lastAttackTime = Time.time;
     }
 
     // 적 AI의 초기 스펙을 결정하는 셋업 메서드
-    public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor) {
+    public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor)
+    {
+        startingHealth = newHealth;
+        health = newHealth;
+
+        damage = newDamage;
+
+        pathFinder.speed = newSpeed;
+
+        enemyRenderer.material.color = skinColor;
     }
 
-    private void Start() {
+    private void Start()
+    {
         // 게임 오브젝트 활성화와 동시에 AI의 추적 루틴 시작
         StartCoroutine(UpdatePath());
     }
 
-    private void Update() {
+    private void Update()
+    {
         // 추적 대상의 존재 여부에 따라 다른 애니메이션을 재생
         enemyAnimator.SetBool("HasTarget", hasTarget);
     }
 
     // 주기적으로 추적할 대상의 위치를 찾아 경로를 갱신
-    private IEnumerator UpdatePath() {
+    private IEnumerator UpdatePath()
+    {
         // 살아있는 동안 무한 루프
         while (!dead)
         {
             // 0.25초 주기로 처리 반복
+            if (hasTarget)
+            {
+                pathFinder.isStopped = false;
+                pathFinder.SetDestination(targetEntity.transform.position);
+            }
+            else
+            {
+                pathFinder.isStopped = true;
+                List<Collider> colliders = new(Physics.OverlapSphere(transform.position, 20f, whatIsTarget));
+
+                foreach (Collider collider in colliders)
+                {
+                    var livingEntity = collider.GetComponent<LivingEntity>();
+
+                    if(livingEntity != null && !livingEntity.dead)
+                    {
+                        targetEntity = livingEntity;
+
+                        break;
+                    }
+                }
+            }
             yield return new WaitForSeconds(0.25f);
         }
     }
 
     // 데미지를 입었을때 실행할 처리
-    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal) {
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
         // LivingEntity의 OnDamage()를 실행하여 데미지 적용
+        if(!dead)
+        {
+            hitEffect.transform.position = hitPoint;
+            hitEffect.transform.rotation = Quaternion.LookRotation(hitNormal);
+            hitEffect.Play();
+
+            enemyAudioPlayer.PlayOneShot(hitSound);
+        }
         base.OnDamage(damage, hitPoint, hitNormal);
     }
 
     // 사망 처리
-    public override void Die() {
+    public override void Die()
+    {
         // LivingEntity의 Die()를 실행하여 기본 사망 처리 실행
         base.Die();
+
+        Collider[] colliders = GetComponents<Collider>();
+        foreach(Collider collider in colliders)
+        {
+            collider.enabled = false;
+        }
+
+        pathFinder.isStopped = true;
+        pathFinder.enabled = false;
+
+        enemyAnimator.SetTrigger("Die");
+        enemyAudioPlayer.PlayOneShot(deathSound);
     }
 
-    private void OnTriggerStay(Collider other) {
+    private void OnTriggerStay(Collider other)
+    {
         // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행   
+        if(!dead&& Time.time >= lastAttackTime + timeBetAttack)
+        {
+            var attackTarget = other.GetComponent<LivingEntity>();
+
+            if(attackTarget != null && attackTarget == targetEntity)
+            {
+                lastAttackTime = Time.time;
+
+                Vector3 hitPoint = other.ClosestPoint(transform.position);
+                Vector3 hitNormal = transform.position - other.transform.position;
+
+                attackTarget.OnDamage(damage, hitPoint, hitNormal);
+            }
+        }
     }
 }
